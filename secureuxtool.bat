9ebfe9fcd9c3d46ee372d39bb065cf8eafc3ebfb
Matisio <mateuszmatisio@gmail.com>
2025-05-13 14:53:31 +0200

    Update secureuxtool.bat

--- secureuxtool.bat
+++ secureuxtool.bat
@@ -16,22 +16,25 @@ powershell -Command "Invoke-WebRequest -Uri '%ICON_URL%' -OutFile '%ZIP_PATH%'"
 echo [3/6] Rozpakowywanie...
 powershell -Command "Expand-Archive -Path '%ZIP_PATH%' -DestinationPath '%ICON_DIR%' -Force"
 
-echo [4/6] Ustawianie ikon systemowych...
+REM Ustawienie ikon niestandardowych dla folderów systemowych
 
-:: Komputer
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{20D04FE0-3AEA-1069-A2D8-08002B30309D}" /v DefaultIcon /t REG_SZ /d "%ICON_DIR%\Computer.ico" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{20D04FE0-3AEA-1069-A2D8-08002B30309D}\DefaultIcon" /ve /d "C:\RevertMetro\Ikony\Computer.ico,0" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{56784854-C6CB-462b-8169-88E3790CABC4}\DefaultIcon" /ve /d "C:\RevertMetro\Ikony\Contacts.ico,0" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{B4BFCC3A-DB2C-4244-B029-7FE99A87C641}\DefaultIcon" /ve /d "C:\RevertMetro\Ikony\Desktop.ico,0" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{A8CDFF1C-4878-43be-B5FD-F8091C1C60D9}\DefaultIcon" /ve /d "C:\RevertMetro\Ikony\Documents.ico,0" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{374DE55D-E6AF-4A3E-B4F9-C03ABD3D2A5E}\DefaultIcon" /ve /d "C:\RevertMetro\Ikony\Downloads.ico,0" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{17CD9488-1228-4B2F-88CE-4298E93E0966}\DefaultIcon" /ve /d "C:\RevertMetro\Ikony\Favourites.ico,0" /f
+reg add "HKCR\Folder\DefaultIcon" /ve /d "C:\RevertMetro\Ikony\Folder.ico,0" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{AF5CB166-0081-4757-8C04-B104B683199C}\DefaultIcon" /ve /d "C:\RevertMetro\Ikony\Games.ico,0" /f
+reg add "HKCR\lnkfile\DefaultIcon" /ve /d "C:\RevertMetro\Ikony\Links.ico,0" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{3dfdf296-dbec-4fab-a6c3-4607cf714165}\DefaultIcon" /ve /d "C:\RevertMetro\Ikony\Music.ico,0" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{F02C1A0D-BE21-4350-88B0-7367FC964101}\DefaultIcon" /ve /d "C:\RevertMetro\Ikony\Network.ico,0" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{BDEADF00-C265-11D0-BECD-00C04FD8D503}\DefaultIcon" /ve /d "C:\RevertMetro\Ikony\Pictures.ico,0" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{0E270DAA-1BE6-48F2-AC49-6FA898E249C0}\DefaultIcon" /ve /d "C:\RevertMetro\Ikony\Searches.ico,0" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{59031A47-3F72-444A-89C5-5595FE6B30EE}\DefaultIcon" /ve /d "C:\RevertMetro\Ikony\UserFolder.ico,0" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{F86FA3AB-70D2-4FC7-9C99-4106C05ED162}\DefaultIcon" /ve /d "C:\RevertMetro\Ikony\Videos.ico,0" /f
 
-:: Sieć
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{F02C1A0D-BE21-4350-88B0-7367FC96EF3C}" /v DefaultIcon /t REG_SZ /d "%ICON_DIR%\Network.ico" /f
-
-:: Kosz - pusty
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}" /v empty /t REG_SZ /d "%ICON_DIR%\RecycleEmpty.ico" /f
-
-:: Kosz - pełny
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}" /v full /t REG_SZ /d "%ICON_DIR%\RecycleFull.ico" /f
-
-:: Folder użytkownika
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{59031a47-3f72-44a7-89c5-5595fe6b30ee}" /v DefaultIcon /t REG_SZ /d "%ICON_DIR%\UserFolder.ico" /f
+echo Ikony zostały zaktualizowane. Uruchom ponownie Eksplorator Windows, aby zobaczyć zmiany.
 
 echo [5/6] Restartowanie Eksploratora Windows...
 taskkill /f /im explorer.exe >nul
