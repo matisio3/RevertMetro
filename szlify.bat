0b3fa9f023076fa9231cb35f2f07c52f2f7b1ecc
Matisio <mateuszmatisio@gmail.com>
2025-05-14 20:38:07 +0200

    Update szlify.bat

--- szlify.bat
+++ szlify.bat
@@ -1,6 +1,7 @@
 @echo off
 reg add "HKCU\Software\Policies\Microsoft\Windows\Explorer" /v DisableNotificationCenter /t REG_DWORD /d 1 /f
 reg delete HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v StartScript /f
+del "C:\Users\Matisio\AppData\Roaming\czesc_3.bat" /s /q
 taskkill /f /im explorer.exe
 start explorer.exe
 mshta.exe "javascript:alert('Instalacja zostala zakonczona');close();"
