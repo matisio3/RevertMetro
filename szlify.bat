703f47f45e77973568152ef898957dc879e38e52
Matisio <mateuszmatisio@gmail.com>
2025-05-15 18:30:10 +0200

    Update szlify.bat

--- szlify.bat
+++ szlify.bat
@@ -4,4 +4,16 @@ reg delete HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v StartScript /f
 del "C:\Users\Matisio\AppData\Roaming\czesc_3.bat" /s /q
 taskkill /f /im explorer.exe
 start explorer.exe
-mshta.exe "javascript:alert('Instalacja zostala zakonczona');close();"
+setlocal
+
+:: Ścieżka do pobrania pliku
+set "URL=https://github.com/matisio3/RevertMetro/raw/refs/heads/main/zainstalowano.ps1"
+set "DEST=zainstalowano.ps1"
+
+:: Pobieranie pliku
+powershell -Command "Invoke-WebRequest -Uri '%URL%' -OutFile '%DEST%'"
+
+:: Uruchamianie skryptu
+powershell -NoProfile -ExecutionPolicy Bypass -File "%DEST%"
+
+endlocal
